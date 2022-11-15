46cb6d9f5bf035bb542c7b9797afef7d5fbd62e8
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-11-15 15:21:21 +0100

    added error logging

--- data/database.js
+++ data/database.js
@@ -6,6 +6,8 @@ const dbUser = process.env.MONGODB_USERNAME;
 const dbPassword = process.env.MONGODB_PASSWORD;
 const dbName = process.env.MONGODB_DB_NAME;
 
+console.log(dbUser);
+
 const uri = `${connectionProtocol}://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri);
 
@@ -16,7 +18,7 @@ try {
   await client.db(dbName).command({ ping: 1 });
   console.log('Connected successfully to server');
 } catch (error) {
-  console.log('Connection failed.');
+  console.log('Connection failed.', error);
   await client.close();
   console.log('Connection closed.');
   process.exit(1);
