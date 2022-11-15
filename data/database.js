0b7bd838f4619e8b995890b8e398436568df11f2
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-11-15 15:15:30 +0100

    Revert "added dotenv"
    This reverts commit 307c4f236b1b5961ed4ee1bbc965c5a412bda49f.

--- data/database.js
+++ data/database.js
@@ -1,6 +1,4 @@
 import { MongoClient } from 'mongodb';
-import * as dotenv from 'dotenv';
-dotenv.config();
 
 const connectionProtocol = process.env.MONGODB_CONNECTION_PROTOCOL;
 const clusterAddress = process.env.MONGODB_CLUSTER_ADDRESS;
