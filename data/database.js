28ee0ae414b46469aeaf3700d9da11c08b86de38
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-11-15 15:25:48 +0100

    put test job in container

--- data/database.js
+++ data/database.js
@@ -6,8 +6,6 @@ const dbUser = process.env.MONGODB_USERNAME;
 const dbPassword = process.env.MONGODB_PASSWORD;
 const dbName = process.env.MONGODB_DB_NAME;
 
-console.log(dbUser);
-
 const uri = `${connectionProtocol}://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri);
 
@@ -18,7 +16,7 @@ try {
   await client.db(dbName).command({ ping: 1 });
   console.log('Connected successfully to server');
 } catch (error) {
-  console.log('Connection failed.', error);
+  console.log('Connection failed.');
   await client.close();
   console.log('Connection closed.');
   process.exit(1);
